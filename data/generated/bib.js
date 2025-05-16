ee89c9c08d590759049fb2bf3d5e5971fe8c81e2
fish0928 <Erica1105Yu@outlook.com>
2025-05-16 22:35:32 +0800

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,114 @@
+﻿define({ entries : {
+    "Firat2020": {
+        "abstract": "IEEE Xplore Part Number: CFP20V66-ART.",
+        "isbn": "9781728141084",
+        "publisher": "[IEEE]",
+        "title": "Proceedings of the International Conference on Electronics and Sustainable Communication Systems (ICESC 2020) : 02-04, July 2020",
+        "type": "book",
+        "year": "2020"
+    },
+    "Gupta2020": {
+        "abstract": "Annotation International Conference on Next Generation Intelligent Systems(ICNGIS) focuses to explore the latest technologies, issues, trends, strategies and the challenges that are faced by the professionals and to turn these issues into smart solutions viable for the next generation systems. The conference offers an opportunity for participants to showcase their research findings, discuss latest advancement in analytical, numerical and experimental techniques to tackle cutting edge research problems, to identify key trends and issues confronting future research, and pursue effective collaboration in R&D.",
+        "isbn": "9781509008704",
+        "pages": "417",
+        "publisher": "IEEE",
+        "title": "2016 International Conference on Next Generation Intelligent Systems (ICNGIS) : September 1-3, 2016 in Kottayam, India",
+        "type": "book",
+        "year": "2016"
+    },
+    "HasanMBA2024": {
+        "abstract": "| ABSTRACT This research explored movie recommendation systems based on predicting top-rated and suitable movies for users. This research proposed a hybrid movie recommendation system that integrates both text-to-number conversion and cosine similarity approaches to predict the most top-rated and desired movies for the targeted users. The proposed movie recommendation employed the Alternating Least Squares (ALS) algorithm to reinforce the accuracy of movie recommendations. The performance analysis and evaluation were undertaken by employing the widely used \"TMDB 5000 Movie Dataset\" from the Kaggle dataset. Two experiments were conducted, categorizing the dataset into distinct modules, and the outcomes were contrasted with state-of-the-art models. The first experiment attained a Root Mean Squared Error (RMSE) of 0.97613, while the second experiment expanded predictions to 4800 movies, culminating in a substantially minimized RMSE of 0.8951, portraying a 97% accuracy enhancement. The findings underscore the essence of parameter selection in text-to-number conversion and cosine and the gap for other systems to maintain user preferences for comprehensive and precise data gathering. Overall, the proposed hybrid movie recommendation system demonstrated promising results in predicting top-rated movies and offering personalized and accurate recommendations to users. |",
+        "author": "Rokibul Hasan MBA and Janatul Ferdous",
+        "doi": "10.32996/jcsts",
+        "keywords": "Alternating Least Squares,Cosine Similarity | ARTICLE INFORMATION,Hybrid Movie Recommendation,Movie Recommendation System,Text-to-Data-Conversion",
+        "title": "Dominance of AI and Machine Learning Techniques in Hybrid Movie Recommendation System Applying Text-to-number Conversion and Cosine Similarity Approaches",
+        "type": "article",
+        "url": "https://creativecommons.org/licenses/by/4.0/",
+        "year": "2024"
+    },
+    "Jain2023": {
+        "abstract": "We are living in a digital era, nowadays everything starting from shopping to entertainment is easily accessible online to all the age groups, thanks to the user-friendly interface. This technological advancement was maximized during COVID-19 and continues to flourish till now. But this development made about 29.1 percent of the population that consists of children under the age of 18 vulnerable to movies, shows having illegal and adult content. Not only this, even for adults it can be a tiresome process to scroll through a long list of movies on sites just to find that one movie of your taste. In such cases, a recommendation system will help the user filter out the content easily. In this paper, we have done detailed data analysis over the IMDB movie dataset and created a recommendation system where users can look for movies on the basis of actor, director, genre, language etc. This system works using the concept of correlation and feature engineering.",
+        "author": "Digha Jain and Atishay Jain and Sudhanshu Gonge and Rahul Joshi and Sonali Kothari and Ketan Kotecha",
+        "booktitle": "2023 International Conference on Computer, Electronics and Electrical Engineering and their Applications, IC2E3 2023",
+        "doi": "10.1109/IC2E357697.2023.10262669",
+        "isbn": "9798350338003",
+        "keywords": "Data pre-processing,correlation,cosine based similarity,movies,recommendation system,recommender system,visualizations",
+        "publisher": "Institute of Electrical and Electronics Engineers Inc.",
+        "title": "Movie Recommendation System and Data Analysis",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "Kumar2020": {
+        "abstract": "Recommendation systems (RSs) have garnered immense interest for applications in e-commerce and digital media. Traditional approaches in RSs include such as collaborative filtering (CF) and content-based filtering (CBF) through these approaches that have certain limitations, such as the necessity of prior user history and habits for performing the task of recommendation. To minimize the effect of such limitation, this article proposes a hybrid RS for the movies that leverage the best of concepts used from CF and CBF along with sentiment analysis of tweets from microblogging sites. The purpose to use movie tweets is to understand the current trends, public sentiment, and user response of the movie. Experiments conducted on the public database have yielded promising results.",
+        "author": "Sudhanshu Kumar and Kanjar De and Partha Pratim Roy",
+        "doi": "10.1109/TCSS.2020.2993585",
+        "issn": "2329924X",
+        "issue": "4",
+        "journal": "IEEE Transactions on Computational Social Systems",
+        "keywords": "Collaborative filtering,Twitter,content-based filtering,recommendation system (RS),sentiment analysis",
+        "month": "8",
+        "pages": "915-923",
+        "publisher": "Institute of Electrical and Electronics Engineers Inc.",
+        "title": "Movie Recommendation System Using Sentiment Analysis from Microblogging Data",
+        "type": "article",
+        "volume": "7",
+        "year": "2020"
+    },
+    "Li2016": {
+        "abstract": "The emergence of the online media sharing sites (e.g. Youtube, Youku, and Hulu) have introduced new challenges in program recommendation in online networks. However, there is a bottleneck that the amount of available viewing logs and user friendship networks are too limited to design effective recommendation algorithms. Thus, carrying out an intelligent program recommendation system is important for these sites. In this work, we propose a novel model which turns to the social networks and mine user preferences information expressed in microblogs for evaluating the similarity between online movies and TV episodes. To the best of our knowledge, it is the first effort to bridge the gap between movie and TV watchers domain with social network activities. Moreover, it is the first approach that can solve the \u201ccold-start\u201d problem in movie and TV recommendation system. Series of data mining approaches and social computing models have been adopted in this work. Similar programs found from the social network are further used to suggest programs in other media devices. This work can be easily applied in online media streaming sites in order that intelligent recommendations of programs can be made to the customers through mining microblogs.",
+        "author": "Hui Li and Jiangtao Cui and Bingqing Shen and Jianfeng Ma",
+        "doi": "10.1016/j.neucom.2015.09.134",
+        "issn": "18728286",
+        "journal": "Neurocomputing",
+        "keywords": "Behavior analysis,Collaborative filtering,Knowledge discovery,Microblog,Recommendation",
+        "month": "10",
+        "pages": "164-173",
+        "publisher": "Elsevier B.V.",
+        "title": "An intelligent movie recommendation system through group-level sentiment analysis in microblogs",
+        "type": "article",
+        "volume": "210",
+        "year": "2016"
+    },
+    "Petras2020": {
+        "abstract": "Conference held online due to COVID-19.",
+        "author": "Ivo. Petra\u0301s\u030c and Ja\u0301n. Kac\u030cur",
+        "isbn": "9781728119519",
+        "publisher": "IEEE",
+        "title": "Proceedings of the 2020 21st International Carpathian Control Conference (ICCC) : virtual conference, Kos\u030cice, Slovak Republic, October 27-29, 2020",
+        "type": "book",
+        "year": "2020"
+    },
+    "Taj2024": {
+        "abstract": "An Advanced Recommendation System is a system that provides accurate and efficient suggestions to the users for certain resources like books, medicines, movies, songs, etc., based on the specific data set containing the previously used resources by the users. The Movie recommendation system predicts what movies a user will enjoy based on the attributes of the previously watched movies by the user. This system leverages the historical user interactions with movies to predict the preferences and recommend the relevant content. Many critical attributes can be considered while designing a movie recommendation system like the genre of the movie, actors, crew, the director of the movie. The systems accuracy to recommend movies can be improved by a combination of two or more of these attributes.The Recommendation systems are particularly valuable for organizations that collect extensive customer data and aim to optimize their recommendations. They play a pivotal role in enhancing user experience by providing personalized suggestions for movies. The research work aims to build an advanced, stable and accurate recommendation system, which will be done by using a hybrid version of Content-based filtering and Cosine Similarity. Additionally, the system proposed is user friendly by providing the option to download the latest recommended movies dataset according to their popularity, vote count, likes and ratings. In this paper, we present an in-depth analysis of our proposed Movie Recommendation system, related work, proposed work, result analysis and conclusions.",
+        "author": "Nousheen Taj and M. H. Varun and V. Navya",
+        "booktitle": "Proceedings of the 5th International Conference on Smart Electronics and Communication, ICOSEC 2024",
+        "doi": "10.1109/ICOSEC61587.2024.10722333",
+        "isbn": "9798331504403",
+        "keywords": "Advanced movie recommendation,Content-based filtering,Cosine similarity,Hybrid novel approach,Streamlit",
+        "pages": "1693-1698",
+        "publisher": "Institute of Electrical and Electronics Engineers Inc.",
+        "title": "Advanced Content-based Movie Recommendation System",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "Wang2018": {
+        "abstract": "Movie recommendation in mobile environment is critically important for mobile users. It carries out comprehensive aggregation of user's preferences, reviews, and emotions to help them find suitable movies conveniently. However, it requires both accuracy and timeliness. In this paper, a movie recommendation framework based on a hybrid recommendation model and sentiment analysis on Spark platform is proposed to improve the accuracy and timeliness of mobile movie recommender system. In the proposed approach, we first use a hybrid recommendation method to generate a preliminary recommendation list. Then sentiment analysis is employed to optimize the list. Finally, the hybrid recommender system with sentiment analysis is implemented on Spark platform. The hybrid recommendation model with sentiment analysis outperforms the traditional models in terms of various evaluation criteria. Our proposed method makes it convenient and fast for users to obtain useful movie suggestions.",
+        "author": "Yibo Wang and Mingming Wang and Wei Xu",
+        "doi": "10.1155/2018/8263704",
+        "issn": "15308677",
+        "journal": "Wireless Communications and Mobile Computing",
+        "publisher": "Hindawi Limited",
+        "title": "A Sentiment-Enhanced Hybrid Recommender System for Movie Recommendation: A Big Data Analytics Framework",
+        "type": "article",
+        "volume": "2018",
+        "year": "2018"
+    },
+    "Wu2018": {
+        "isbn": "9781538665657",
+        "pages": "125",
+        "publisher": "Institute of Electrical and Electronics Engineers",
+        "title": "2018 IEEE 9th International Conference on Software Engineering and Service Science : 23-25 November 2018, Beijing, China",
+        "type": "book",
+        "year": "2018"
+    }
+}});
